a3b054ded27ebc4f432ad50fbcfcea1c206e5c90
Exenifix <yeligar@ya.ru>
2022-11-26 20:41:40 +0300

    [patch] Offset-naive datetimes in stats

--- ext/tracking.py
+++ ext/tracking.py
@@ -102,8 +102,8 @@ class ActivityTracking(Cog):
         period_end: str,
     ):
         try:
-            start = pendulum.parse(period_start, strict=False)
-            end = pendulum.parse(period_end, strict=False)
+            start = pendulum.parse(period_start, strict=False, tz=None)
+            end = pendulum.parse(period_end, strict=False, tz=None)
             if not 2 <= (end - start).hours <= 154:
                 await inter.send(
                     "The duration must be more than 2 hours and less than 7 days",
